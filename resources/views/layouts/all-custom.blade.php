730ebc8251f549b9870597e5b4b80950b047ef5b
ghader <ansari.it94@gmail.com>
2020-02-19 09:37:50 +0330

    change font wizard form in orders all custom blade in layout

---
+++ resources/views/layouts/all-custom.blade.php
@@ -0,0 +1,71 @@
+
+{{--<link href="{{asset('vendors/bootstrap/dist/css/bootstrap.min.css')}}" rel="stylesheet">--}}
+{{--<link href="{{asset('vendors/bootstrap-rtl/dist/css/bootstrap-rtl.min.css')}}" rel="stylesheet">--}}
+<!-- Font Awesome -->
+<link href="{{asset('vendors/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
+<!-- NProgress -->
+<link href="{{asset('vendors/nprogress/nprogress.css')}}" rel="stylesheet">
+<!-- bootstrap-progressbar -->
+<link href="{{asset('vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css')}}" rel="stylesheet">
+<!-- iCheck -->
+<link href="{{asset('vendors/iCheck/skins/flat/green.css')}}" rel="stylesheet">
+<!-- bootstrap-daterangepicker -->
+<link href="{{asset('vendors/bootstrap-daterangepicker/daterangepicker.css')}}" rel="stylesheet">
+<!-- Dropzone.js -->
+<link href="{{('vendors/dropzone/dist/min/dropzone.min.css')}}" rel="stylesheet">
+
+<!-- bootstrap-wysiwyg -->
+<link href="{{asset('vendors/google-code-prettify/bin/prettify.min.css')}}" rel="stylesheet">
+<!-- Select2 -->
+<link href="{{asset('vendors/select2/dist/css/select2.min.css')}}" rel="stylesheet">
+<!-- Switchery -->
+<link href="{{asset('vendors/switchery/dist/switchery.min.css')}}" rel="stylesheet">
+<!-- starrr -->
+<link href="{{asset('vendors/starrr/dist/starrr.css')}}" rel="stylesheet">
+
+<!-- Custom Theme Style -->
+<link href="{{asset('build/css/custom.min.css')}}" rel="stylesheet">
+
+<!-- jQuery -->
+<script src="{{asset('vendors/jquery/dist/jquery.min.js')}}"></script>
+<!-- Bootstrap -->
+<script src="{{asset('vendors/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+<!-- FastClick -->
+<script src="{{asset('vendors/fastclick/lib/fastclick.js')}}"></script>
+<!-- NProgress -->
+<script src="{{asset('vendors/nprogress/nprogress.js')}}"></script>
+<!-- bootstrap-progressbar -->
+<script src="{{asset('vendors/bootstrap-progressbar/bootstrap-progressbar.min.js')}}"></script>
+<!-- iCheck -->
+<script src="{{asset('vendors/iCheck/icheck.min.js')}}"></script>
+
+<!-- bootstrap-daterangepicker -->
+<script src="{{asset('vendors/moment/min/moment.min.js')}}"></script>
+
+<script src="{{asset('vendors/bootstrap-daterangepicker/daterangepicker.js')}}"></script>
+
+<!-- jQuery Smart Wizard -->
+<script src="{{asset('vendors/jQuery-Smart-Wizard/js/jquery.smartWizard.js')}}"></script>
+
+<!-- Custom Theme Scripts -->
+<script src="{{asset('build/js/custom.min.js')}}"></script>
+<!-- Dropzone.js -->
+<script src="{{asset('vendors/dropzone/dist/min/dropzone.min.js')}}"></script>
+
+<!-- Custom Theme Scripts -->
+<script>
+
+    Dropzone.options.myAwesomeDropzone = {
+        dictDefaultMessage: "فایل را اینجا رها کنید تا آپلود شود",
+        dictFallbackMessage: "مرورگر شما آپلود فایل drag'n'drop را پشتیبانی نمی کند.",
+        dictFallbackText: "برای بارگذاری فایل های خود مانند روزهای گذشته، از فرم پشت زیر استفاده کنید.",
+        dictFileTooBig: "پرونده خیلی بزرگ است (MiB). حداکثر اندازه فایل: MiB.",
+        dictInvalidFileType: "شما نمیتوانید فایلهای این نوع آپلود کنید.",
+        dictResponseError: "سرور کد  را در پاسخ داد.",
+        dictCancelUpload: "لغو آپلود",
+        dictCancelUploadConfirmation: "آیا مطمئن هستید که میخواهید این آپلود را لغو کنید؟",
+        dictRemoveFile: "حذف فایل",
+        dictRemoveFileConfirmation: null,
+        dictMaxFilesExceeded: "شما نمیتوانید فایلهای بیشتری آپلود کنید."
+    }
+</script>
---
+++ resources/views/orders/wizard-step1.blade.php
@@ -0,0 +1,13 @@
+<div class="form-group">
+    <h6>در این مرحله دسته بندی را انتخاب نمایید و به مرحله بعد بروید</h6>
+    <label class="control-label col-md-3 col-sm-3 col-xs-12">انتخاب <span class="required">ضروری است</span></label>
+    <div class="col-md-9 col-sm-9 col-xs-12">
+        <select class="form-control" required="required">
+            <option>یک گزینه را انتخاب نمایید</option>
+            <option>فناوری اطلاعات</option>
+            <option>زیر ساخت</option>
+            <option>امنیت</option>
+            <option>سایر</option>
+        </select>
+    </div>
+</div>

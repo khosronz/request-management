5b8cdb82603feb59f589a41b504ec9db8aa4326a
khosro nazari <khosronz.com@gmail.com>
2020-02-29 09:01:30 +0330

    Translate in messages

--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -16,8 +16,7 @@
                              <div class="card-header">
                                  <strong>@lang('Details')</strong>
                                   <a href="{!! route('tickets.index') !!}" class="btn btn-success"><i class="fa fa-arrow-left"></i> @lang('Back to my tickets')</a>
-                                 <a href="{!! route('tickets.show', [$message->ticket_id]) !!}" class='btn btn-ghost-success'><i
-                                             class="fa fa-undo"></i></a>
+                                 <a href="{!! route('tickets.show', [$message->ticket_id]) !!}" class='btn btn-ghost-success'><i class="fa fa-undo"></i> @lang('Back to another messages of this ticket')</a>
                              </div>
                              <div class="card-body">
                                  @include('messages.show_fields')
--- resources/views/tickets/show_fields.blade.php
+++ resources/views/tickets/show_fields.blade.php
@@ -78,10 +78,8 @@
                 <td>
                     {!! Form::open(['route' => ['messages.destroy', $message->id], 'method' => 'delete']) !!}
                     <div class='btn-group'>
-                        <a href="{!! route('messages.show', [$message->id]) !!}" class='btn btn-ghost-success'><i
-                                    class="fa fa-eye"></i></a>
-                        <a href="{!! route('messages.edit', [$message->id]) !!}" class='btn btn-ghost-info'><i
-                                    class="fa fa-edit"></i></a>
+                        <a href="{!! route('messages.show', [$message->id]) !!}" class='btn btn-ghost-success'><i class="fa fa-eye"></i></a>
+                        <a href="{!! route('messages.edit', [$message->id]) !!}" class='btn btn-ghost-info'><i class="fa fa-edit"></i></a>
                         {!! Form::button('<i class="fa fa-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-ghost-danger', 'onclick' => "return confirm('Are you sure?')"]) !!}
                     </div>
                     {!! Form::close() !!}

d5dc51a64d623a9532ac622f13459a77f9a26180
khosronz <khosronz.com@gmail.com>
2020-03-17 14:26:43 +0330

    Contact User To Role in the Show role page

--- resources/views/roles/show.blade.php
+++ resources/views/roles/show.blade.php
@@ -25,4 +25,33 @@
                  </div>
           </div>
     </div>
+     @include('role_users.index-create')
+
+     @php
+         $roleUsers=\App\Models\RoleUser::where('role_id','=',$role->id)->get();
+     @endphp
+
+     <div class="container-fluid">
+         <div class="animated fadeIn">
+             @include('flash::message')
+             <div class="row">
+                 <div class="col-lg-12">
+                     <div class="card">
+                         <div class="card-header">
+                             <i class="fa fa-align-justify"></i>
+                             @lang('Role Users')
+                         </div>
+                         <div class="card-body">
+                             @include('role_users.table')
+                             {{--<div class="pull-left mr-3" dir="ltr">--}}
+
+                                 {{--@include('coreui-templates::common.paginate', ['records' => $roleUsers])--}}
+
+                             {{--</div>--}}
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
 @endsection
--- resources/views/roles/show_fields.blade.php
+++ resources/views/roles/show_fields.blade.php
@@ -1,30 +1,33 @@
-<!-- Id Field -->
-<div class="form-group">
-    {!! Form::label('id', __('Id').':') !!}
-    <p>{!! $role->id !!}</p>
-</div>
+<div class="row">
+    <!-- Id Field -->
+    <div class="form-group col-sm-4">
+        {!! Form::label('id', __('Id').':') !!}
+        <p>{!! $role->id !!}</p>
+    </div>
 
-<!-- Title Field -->
-<div class="form-group">
-    {!! Form::label('title', __('Title').':') !!}
-    <p>{!! $role->title !!}</p>
-</div>
+    <!-- Title Field -->
+    <div class="form-group col-sm-4">
+        {!! Form::label('title', __('Title').':') !!}
+        <p>{!! $role->title !!}</p>
+    </div>
 
-<!-- Desc Field -->
-<div class="form-group">
-    {!! Form::label('desc', __('Desc').':') !!}
-    <p>{!! $role->desc !!}</p>
-</div>
+    <!-- Desc Field -->
+    <div class="form-group col-sm-12">
+        {!! Form::label('desc', __('Desc').':') !!}
+        <p>{!! $role->desc !!}</p>
+    </div>
 
-<!-- Created At Field -->
-<div class="form-group">
-    {!! Form::label('created_at', __('Created At').':') !!}
-    <p>{!! jdate($role->created_at) !!}</p>
-</div>
+    <!-- Created At Field -->
+    <div class="form-group col-sm-4">
+        {!! Form::label('created_at', __('Created At').':') !!}
+        <p>{!! jdate($role->created_at) !!}</p>
+    </div>
 
-<!-- Updated At Field -->
-<div class="form-group">
-    {!! Form::label('updated_at', __('Updated At').':') !!}
-    <p>{!! jdate($role->updated_at) !!}</p>
-</div>
+    <!-- Updated At Field -->
+    <div class="form-group col-sm-4">
+        {!! Form::label('updated_at', __('Updated At').':') !!}
+        <p>{!! jdate($role->updated_at) !!}</p>
+    </div>
 
+
+</div>

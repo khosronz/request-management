e3947f6246c92bf5eb9e593c6e2b7b86e7e7055a
khosro nazari <khosronz.com@gmail.com>
2020-04-03 16:12:15 +0430

    Setting translate

--- resources/views/settings/fields.blade.php
+++ resources/views/settings/fields.blade.php
@@ -1,41 +1,44 @@
-<!-- Title Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('title', __('Title').':') !!}
-    {!! Form::text('title', null, ['class' => 'form-control']) !!}
-</div>
+<div class="row">
+    <!-- Title Field -->
+    <div class="form-group col-sm-4">
+        {!! Form::label('title', __('Title').':') !!}
+        {!! Form::text('title', null, ['class' => 'form-control']) !!}
+    </div>
 
-<!-- Default Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('default', __('Default').':') !!}
-    {!! Form::text('default', null, ['class' => 'form-control']) !!}
-</div>
+    <!-- Default Field -->
+    <div class="form-group col-sm-4">
+        {!! Form::label('default', __('Default').':') !!}
+        {!! Form::text('default', null, ['class' => 'form-control']) !!}
+    </div>
 
-<!-- User Id Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('user_id', __('User Id').':') !!}
-    {!! Form::text('user_id', null, ['class' => 'form-control']) !!}
-</div>
+    <!-- User Id Field -->
+    <div class="form-group col-sm-4 sr-only">
+        {!! Form::label('user_id', __('User Id').':') !!}
+        {!! Form::text('user_id', \Illuminate\Support\Facades\Auth::id(), ['class' => 'form-control']) !!}
+    </div>
 
-<!-- Short Code Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('short_code', __('Short Code').':') !!}
-    {!! Form::text('short_code', null, ['class' => 'form-control']) !!}
-</div>
+    <!-- Short Code Field -->
+    <div class="form-group col-sm-4">
+        {!! Form::label('short_code', __('Short Code').':') !!}
+        {!! Form::text('short_code', null, ['class' => 'form-control']) !!}
+    </div>
 
-<!-- Value Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('value', __('Value').':') !!}
-    {!! Form::text('value', null, ['class' => 'form-control']) !!}
-</div>
+    <!-- Value Field -->
+    <div class="form-group col-sm-4">
+        {!! Form::label('value', __('Value').':') !!}
+        {!! Form::text('value', null, ['class' => 'form-control']) !!}
+    </div>
 
-<!-- Desc Field -->
-<div class="form-group col-sm-12 col-lg-12">
-    {!! Form::label('desc', __('Desc').':') !!}
-    {!! Form::textarea('desc', null, ['class' => 'form-control']) !!}
-</div>
+    <!-- Desc Field -->
+    <div class="form-group col-sm-12 col-lg-12">
+        {!! Form::label('desc', __('Desc').':') !!}
+        {!! Form::textarea('desc', null, ['class' => 'form-control']) !!}
+    </div>
 
-<!-- Submit Field -->
-<div class="form-group col-sm-12">
-    {!! Form::submit(__('Save'), ['class' => 'btn btn-primary']) !!}
-    <a href="{!! route('settings.index') !!}" class="btn btn-default">@lang('Cancel')</a>
+    <!-- Submit Field -->
+    <div class="form-group col-sm-12">
+        {!! Form::submit(__('Save'), ['class' => 'btn btn-primary']) !!}
+        <a href="{!! route('settings.index') !!}" class="btn btn-default">@lang('Cancel')</a>
+    </div>
+
 </div>
--- resources/views/settings/show_fields.blade.php
+++ resources/views/settings/show_fields.blade.php
@@ -1,54 +1,57 @@
-<!-- Id Field -->
-<div class="form-group">
-    {!! Form::label('id', __('Id').':') !!}
-    <p>{!! $setting->id !!}</p>
-</div>
+<div class="row">
+    <!-- Id Field -->
+    <div class="form-group col-sm-4">
+        {!! Form::label('id', __('Id').':') !!}
+        <p>{!! $setting->id !!}</p>
+    </div>
 
-<!-- Title Field -->
-<div class="form-group">
-    {!! Form::label('title', __('Title').':') !!}
-    <p>{!! $setting->title !!}</p>
-</div>
+    <!-- Title Field -->
+    <div class="form-group col-sm-4">
+        {!! Form::label('title', __('Title').':') !!}
+        <p>{!! $setting->title !!}</p>
+    </div>
 
-<!-- Default Field -->
-<div class="form-group">
-    {!! Form::label('default', __('Default').':') !!}
-    <p>{!! $setting->default !!}</p>
-</div>
+    <!-- Default Field -->
+    <div class="form-group col-sm-4">
+        {!! Form::label('default', __('Default').':') !!}
+        <p>{!! $setting->default !!}</p>
+    </div>
 
-<!-- User Id Field -->
-<div class="form-group">
-    {!! Form::label('user_id', __('User Id').':') !!}
-    <p>{!! $setting->user_id !!}</p>
-</div>
+    <!-- User Id Field -->
+    <div class="form-group col-sm-4">
+        {!! Form::label('user_id', __('User Id').':') !!}
+        <p>{!! $setting->user->name !!}</p>
+    </div>
 
-<!-- Short Code Field -->
-<div class="form-group">
-    {!! Form::label('short_code', __('Short Code').':') !!}
-    <p>{!! $setting->short_code !!}</p>
-</div>
+    <!-- Short Code Field -->
+    <div class="form-group col-sm-4">
+        {!! Form::label('short_code', __('Short Code').':') !!}
+        <p>{!! $setting->short_code !!}</p>
+    </div>
 
-<!-- Value Field -->
-<div class="form-group">
-    {!! Form::label('value', __('Value').':') !!}
-    <p>{!! $setting->value !!}</p>
-</div>
+    <!-- Value Field -->
+    <div class="form-group col-sm-4">
+        {!! Form::label('value', __('Value').':') !!}
+        <p>{!! $setting->value !!}</p>
+    </div>
 
-<!-- Desc Field -->
-<div class="form-group">
-    {!! Form::label('desc', __('Desc').':') !!}
-    <p>{!! $setting->desc !!}</p>
-</div>
+    <!-- Desc Field -->
+    <div class="form-group col-sm-12">
+        {!! Form::label('desc', __('Desc').':') !!}
+        <p>{!! $setting->desc !!}</p>
+    </div>
 
-<!-- Created At Field -->
-<div class="form-group">
-    {!! Form::label('created_at', __('Created At').':') !!}
-    <p>{!! $setting->created_at !!}</p>
-</div>
+    <!-- Created At Field -->
+    <div class="form-group col-sm-4">
+        {!! Form::label('created_at', __('Created At').':') !!}
+        <p>{!! jdate($setting->created_at) !!}</p>
+    </div>
 
-<!-- Updated At Field -->
-<div class="form-group">
-    {!! Form::label('updated_at', __('Updated At').':') !!}
-    <p>{!! $setting->updated_at !!}</p>
-</div>
+    <!-- Updated At Field -->
+    <div class="form-group col-sm-4">
+        {!! Form::label('updated_at', __('Updated At').':') !!}
+        <p>{!! jdate($setting->updated_at) !!}</p>
+    </div>
 
+
+</div>
